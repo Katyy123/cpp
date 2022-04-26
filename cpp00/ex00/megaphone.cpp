f347c336808fb77fa4e2e12946512119216fd407
Catia Filiberto <cfiliber@e3r3p4.42roma.it>
2022-04-26 19:31:29 +0200

    cpp00 ex00 may be finished. Check

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,20 +6,40 @@
 /*   By: cfiliber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 16:49:12 by cfiliber          #+#    #+#             */
-/*   Updated: 2022/04/12 18:56:26 by cfiliber         ###   ########.fr       */
+/*   Updated: 2022/04/26 19:28:30 by cfiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-//using namespace std;
-
-int main(void)
+int	ft_toupper(int c)
 {
-	char	buff[512];
-	std::cout << "Hello World!" << std::endl;
-	std::cout << "Input a word: ";
-	std::cin >> buff;
-	std::cout << "You typed: [" << buff << "]" << std::endl; 
+	if (c >= 97 && c <= 122)
+		c -= 32;
+	return (c);
+}
+
+int	main(int ac, char **av)
+{
+	int		i;
+	int		j;
+	
+	i = 1;
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (av[i])
+		{
+			j = 0;
+			while (av[i][j])
+			{
+				std::cout << (char)ft_toupper(av[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
 	return 0;
 }
