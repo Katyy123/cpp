89509bb8b193041361b782b65d70c81612d34070
Catia Filiberto <cfiliber@e3r10p7.42roma.it>
2022-06-09 18:20:29 +0200

    cpp02 ex00 done

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -1,1 +1,40 @@
 #include "Fixed.hpp"
+
+Fixed::Fixed(void) : _n(0) {
+
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const & instance) {
+
+	std::cout << "Copy constructor called" << std::endl;
+	*this = instance;
+}
+
+Fixed::~Fixed(void) {
+
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed & Fixed::operator=(Fixed const & instance) {
+
+	std::cout << "Copy ssignment operator called" << std::endl;
+	if (this != &instance)
+		this->_n = instance.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits( void ) const {
+
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_n;
+}
+
+void Fixed::setRawBits( int const raw ) {
+
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_n = raw;
+}
+
+
+const int Fixed::_FractBits = 8;
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -1,6 +1,23 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+# include <iostream>
 
+class Fixed {
+
+	public:
+		Fixed();
+		Fixed(Fixed const & instance);
+		~Fixed();
+		Fixed & operator=(Fixed const & instance);
+
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+
+	private:
+		int	_n;
+		static const int _FractBits;
+
+};
 
 #endif
