8bc9b006fb0f3cd18ed6ac69e4d3c7c1d26ef2df
Catia Filiberto <cfiliber@e3r6p2.42roma.it>
2022-06-14 16:25:38 +0200

    removed some comments

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: cfiliber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 18:48:23 by cfiliber          #+#    #+#             */
-/*   Updated: 2022/06/13 17:19:03 by cfiliber         ###   ########.fr       */
+/*   Updated: 2022/06/14 15:21:35 by cfiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,68 +72,3 @@ bool bsp( Point const a, Point const b, Point const c, Point const point) {
 	std::cout << "Point on the left or the right of the triangle" << std::endl;
 	return 0;
 }
-	
-
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-// 	int 	cross_count = 0;
-// 	Fixed	check_x;
-	
-// 	if ((a.get_y() > point.get_y()) != (b.get_y() > point.get_y()))
-// 	{
-// 		check_x = (b.get_x() - a.get_x()) * (point.get_y() - a.get_y()) / (b.get_y() - a.get_y()) + a.get_x();
-// 		if (point.get_x() < check_x)
-// 			cross_count++;
-// 	}
-
-
-// 	if ((b.get_y() > point.get_y()) != (c.get_y() > point.get_y()))
-// 	{
-// 		check_x = (c.get_x() - b.get_x()) * (point.get_y() - b.get_y()) / (c.get_y() - b.get_y()) + b.get_x();
-// 		if (point.get_x() < check_x)
-// 			cross_count++;
-// 	}
-
-// 	if ((c.get_y() > point.get_y()) != (a.get_y() > point.get_y()))
-// 	{
-// 		check_x = (a.get_x() - c.get_x()) * (point.get_y() - c.get_y()) / (a.get_y() - c.get_y()) + c.get_x();
-// 		if (point.get_x() < check_x)
-// 			cross_count++;
-// 	}
-	
-// 	return (cross_count % 2 == 1);
-// }
