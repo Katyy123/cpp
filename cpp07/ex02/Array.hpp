f0a39185d8a9d931d6ccb0b99ae5066104f6ba55
Catia Filiberto <catia.filiberto@gmail.com>
2022-09-10 08:10:40 +0200

    almost done: write main

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -2,61 +2,82 @@
 # define ARRAY_HPP
 
 #include <iostream>
+#include <exception>
 
 template< typename T >
 class Array {
 
     public:
-        Array() : _array(NULL), _size(0) {}
-        Array(unsigned int n) : _size(n) {
-            // try {
-            //     if (!(n >= 0 && n <= 4294967295))
-            //         throw std::exception();
-                this->_array = new T[_size];
-            // }
-            // catch (std::exception & e) {
-            //     std::cout << "Invalid size: " << e.what() << std::endl;
-            // }
+        Array() : _array(NULL), _size(0) {
+            std::cout << "Default constructor called" << std::endl;
         }
-        // Array(T const & array) {
 
-        // }
+        Array(unsigned int n) : _array(NULL), _size(n) {
+            this->_array = new T[_size];
+            std::cout << "Parametric constructor called" << std::endl;
+        }
+
+        Array(Array const & array) {
+            this->_array = new T[array._size];
+            *this = array;
+            std::cout << "Copy constructor called" << std::endl;
+        }
 
         ~Array() {
             delete [] this->_array;
+            std::cout << "Destructor called" << std::endl;
         }
+
+        class OutOfBoundsException: public std::exception {
+            virtual const char* what() const throw() {
+                return "ArrayException: index out of bounds";
+            }
+        };
 
         Array & operator=(Array const & array) {
-
+            if (this != &array) {
+                this->_size = array._size;
+                delete [] this->_array;
+                this->_array = new T[_size];
+                for (unsigned int i = 0; i < _size; i++)
+                    this->_array[i] = array._array[i];
+            }
+            std::cout << "Assignment operator called" << std::endl;
+            return *this;
         }
 
-        T & operator[](unsigned int index) {
-
-            try {
-                if (!(index >= 0 && index < this->_size ))
-                    throw std::exception();
-                //return (this->_array[index]);
-            }
-            catch (std::exception & e) {
-                std::cout << "Index out of bounds: " << e.what() << std::endl;
-                return (this->_array[this->_size - 1]);
-            }
-            //return (this->_array[index]);
-            return (this->_array[this->_size - 1]);
+        T & operator[](unsigned int index) const {
+            if (index < 0 || index >= this->_size)
+                throw Array::OutOfBoundsException();
+            return (this->_array[index]);
         }
 
         unsigned int size(void) const {
             return this->_size;
         }
 
-        T* getArrayAddress(void) const {
-            return this->_array;
-        }
+        // T* getArrayAddress(void) const {
+        //     return this->_array;
+        // }
 
     private:
         T*              _array;
         unsigned int    _size;
 
 };
+
+template< typename T>
+std::ostream & operator<<(std::ostream & o, Array<T> const & array) {
+    if (array.size() > 0) {
+        o << "Elements of the array: { ";
+        for (unsigned int i = 0; i < array.size() - 1; i++)
+            o << array[i] << ", ";
+        o << array[array.size() - 1];
+        o << " }" << std::endl;
+    }
+    else
+        o << "No elements" << std::endl;
+    return o;
+}
 
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -68,15 +68,72 @@ int main(void) {
 
     // delete a;
 
-    Array<int> arr1(5);
-    Array<int> arr2();
+    //Array< Array<int> > arr1(3);
+    Array<std::string> arr3(3);
+    //Array<int> arr1(3);
+    // Array<char const *> arr1(3);
+    // Array<char const *> arr2(2);
+    
 
-    std::cout << "_size: " << arr1.size() << std::endl;
-    std::cout << "First element of the _array 1: " << arr1[0] << std::endl;
-    //std::cout << "First element of the _array 2: " << arr2[0] << std::endl;
+    //Array< Array<int> > arr2(3);
 
-    arr1[4] = 37;
-    arr1[5] = 4;
-    std::cout << arr1[5] << std::endl;
+    // arr1[0] = 'a';
+    // arr1[1] = 'b';
+    //arr1[-2] = 'c';
+
+    //arr1[3] = 1;
+    // arr1[1] = 2;
+    // arr1[2] = 3;
+
+    // arr1[0] = "hey";
+    // arr1[1] = "ciao";
+    // arr1[2] = "mondo";
+
+    // arr1[0] = Array<int>(2);
+    // arr1[1] = Array<int>(2);
+    // arr1[2] = Array<int>(2);
+
+    // arr1[0][0] = 8;
+
+    // Array< Array<int> > arr2(arr1);
+    //Array<int> arr2(arr1);
+
+    // arr2[0] = Array<int>(2);
+    // arr2[1] = Array<int>(2);
+    // arr2[2] = Array<int>(2);
+
+    
+
+    std::cout << "arr3: " << arr3 << std::endl;
+
+    //Array<char const *> arr2(arr1);
+    //Array<int> arr2(arr1);
+    //arr2 = arr1;
+    
+
+    //std::cout << "arr2: " << arr2 << std::endl;
+
+    //arr1[0] = "bye";
+    //arr1[0] = 4;
+    //arr1[0][0] = 7;
+    
+
+    // std::cout << "arr1: " << arr1 << std::endl;
+    // std::cout << "arr2: " << arr2 << std::endl;
+
+    // arr1[0] = Array<int>(2);
+    // arr1[1] = Array<int>(2);
+    // arr1[2] = Array<int>(2);
+
+    std::cout << "size: " << arr3.size() << std::endl;
+    //std::cout << "Array address: " << arr1.getArrayAddress() << std::endl;
+
+    // std::cout << arr1 << std::endl;
+    //std::cout << arr2 << std::endl;
+
+    //arr1[4] = 37;
+    // arr1[5] = 4;
+    // std::cout << arr1[5] << std::endl;
+
     
 }
