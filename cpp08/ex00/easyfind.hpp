03b2ab7238f3272e7147e395031467ccfebfb461
Catia Filiberto <catia.filiberto@gmail.com>
2022-09-12 01:14:27 +0200

    done

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,9 +1,27 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-template< typename T >
-void easyfind(T const & x, int n) {
+# include <algorithm>
+# include <iterator>
+# include <exception>
 
+class NotFoundException : public std::exception {
+
+    public:
+        virtual const char * what() const throw() {
+            return "Value not found";
+        }
+};
+
+template< typename T >
+typename T::const_iterator easyfind(T const & x, int n) {
+
+    typename T::const_iterator it;
+
+    it = find(x.begin(), x.end(), n);
+    if (it == x.end())
+        throw NotFoundException();
+    return it;
 }
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,8 +1,83 @@
 #include "easyfind.hpp"
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <list>
+#include <deque>
+
+#define VALUE 42
 
 int main(void) {
+   
+    {
+        std::vector<int> myCont;
+        std::vector<int>::const_iterator it;
+        std::vector<int>::const_iterator it2;
+        int position = 0;
 
-    
+        myCont.push_back(10);
+        myCont.push_back(21);
+        myCont.push_back(34);
+        myCont.push_back(7);
+        myCont.push_back(VALUE);
+
+        it = easyfind(myCont, VALUE);
+        for (it2 = myCont.begin(); it2 != myCont.end(); it2++)
+        {
+            if (*it2 == VALUE)
+                break;
+            ++position;
+        }
+        if (it2 == it)
+            std::cout << "easyfind() function works well" << std::endl;
+        else
+            std::cout << "easyfind() function does not work well" << std::endl;
+        std::cout << "Position of the value: " << position << std::endl;
+    }
+
+    {
+        std::list<int> myCont;
+        std::list<int>::const_iterator it;
+        std::list<int>::const_iterator it2;
+        int position = 0;
+
+        myCont.push_back(VALUE);
+        myCont.push_back(21);
+        myCont.push_back(VALUE);
+        myCont.push_back(7);
+        myCont.push_back(VALUE);
+
+        it = easyfind(myCont, VALUE);
+        for (it2 = myCont.begin(); it2 != myCont.end(); it2++)
+        {
+            if (*it2 == VALUE)
+                break;
+            ++position;
+        }
+        if (it2 == it)
+            std::cout << "easyfind() function works well" << std::endl;
+        else
+            std::cout << "easyfind() function does not work well" << std::endl;
+        std::cout << "Position of the value: " << position << std::endl;
+    }
+
+    {
+        std::deque<int> myCont;
+        std::deque<int>::const_iterator it;
+        std::deque<int>::const_iterator it2;
+
+        myCont.push_back(20);
+        myCont.push_back(21);
+        myCont.push_back(89);
+        myCont.push_back(7);
+        myCont.push_back(98);
+
+        try {
+        it = easyfind(myCont, VALUE);
+        }
+        catch(std::exception & e) {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+
+    return 0;
 }
