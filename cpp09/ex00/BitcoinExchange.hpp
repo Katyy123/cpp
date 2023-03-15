abb2a0dca838bbc8d5e39dd1acee4c0ae2376188
Catia Filiberto <cfiliber@e3r2p8.42roma.it>
2023-03-15 18:30:51 +0100

    just started

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,12 +6,14 @@
 /*   By: cfiliber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 14:51:37 by cfiliber          #+#    #+#             */
-/*   Updated: 2023/03/15 14:53:09 by cfiliber         ###   ########.fr       */
+/*   Updated: 2023/03/15 18:00:13 by cfiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
+# include <map>
+# include <iostream>
 
 #endif
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,18 @@
 /*   By: cfiliber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 14:51:40 by cfiliber          #+#    #+#             */
-/*   Updated: 2023/03/15 14:51:41 by cfiliber         ###   ########.fr       */
+/*   Updated: 2023/03/15 18:29:14 by cfiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv) {
+
+	try {
+		//open argv[1] file
+	}
+	catch(std::exception & e) {
+		
+	}
+}
