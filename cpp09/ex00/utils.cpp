ea58b5f13fc2267d570c72433b73d0e139716ab2
Catia Filiberto <cfiliber@e3r6p3.42roma.it>
2023-03-22 17:26:11 +0100

    removed some useless comments

--- cpp09/ex00/utils.cpp
+++ cpp09/ex00/utils.cpp
@@ -6,7 +6,7 @@
 /*   By: cfiliber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 19:53:45 by catia             #+#    #+#             */
-/*   Updated: 2023/03/21 16:57:34 by cfiliber         ###   ########.fr       */
+/*   Updated: 2023/03/22 17:25:31 by cfiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,29 +67,14 @@ float	ft_stof(std::string const &str) {
 	}
 	if (dot_pos != str.npos)
 	{
-		//std::cout << "test" << std::endl;
 		dec_str = str.substr(dot_pos + 1, str.npos);
 		i = dec_str.length();
-		//std::cout << "i: " << i << std::endl << "dec_str: " << dec_str << std::endl;
 		while (dec_str[i - 1] >= 48 && dec_str[i - 1] <= 57 && i > 0)
 		{
-			dec_part = (dec_part / 10) + (static_cast<float>(dec_str[i - 1] - 48) / 10);
-			//std::cout << "dec_part: " << dec_part << std::endl; 
+			dec_part = (dec_part / 10) + (static_cast<float>(dec_str[i - 1] - 48) / 10); 
 			i--;
 		}
 	}
-	//std::cout << std::endl << "int_part: " << int_part << std::endl;
-	//std::cout << "dec_part: " << dec_part << std::endl;
 	num = int_part + dec_part;
-	//std::cout << "int_part casted in float: " << static_cast<float>(int_part) << std::endl;
-	//std::cout << "num: " << num << std::endl;
 	return (num);
 }
-
-// float	ft_stof(std::string const & str) {
-	
-// 	std::istringstream ss(str);
-//     float f;
-//     ss >> f;
-//     return static_cast<float>(atof(str.c_str()));
-// }
